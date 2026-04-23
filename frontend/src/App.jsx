1b2a5efafbe74e7649ee024d92a45dbdc7e64871
Bjn2003 <benyamindragon@gmail.com>
2026-04-22 22:01:29 -0400

    [Frontend] creación de archivos elementales

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <img src={reactLogo} alt="React logo" width="100" />
+        <h1>Hola mundo</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
