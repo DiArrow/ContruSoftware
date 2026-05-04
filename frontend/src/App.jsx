a4b84d684a18507cbca05c5db85825859cf8dd09
Benjamin <benyamindragon@gmail.com>
2026-05-04 00:09:12 -0400

    Feat: Contenedores iniciales implementados

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
   return (
     <>
       <section id="center">
-        <img src={reactLogo} alt="React logo" width="100" />
-        <h1>Hola mundo</h1>
+        <div className="cuadro-superior"></div>
+        <div className="cuadro-central"></div>
+        <div className="cuadro-inferior"></div>
       </section>
     </>
   )
