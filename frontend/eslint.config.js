ea594b878dd0caafde51229936f2d0b684acafa2
Camilo Fuentes <camilo.fuentes.01.03.20@gmail.com>
2026-05-14 10:46:23 -0400

    [ci]: implementación de linters

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,18 +1,14 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettierPlugin from 'eslint-plugin-prettier/recommended';
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  { ignores: ['dist', '.eslintrc.cjs'] },
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -22,8 +18,25 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+    settings: { react: { version: '19.2.5' } },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'react/prop-types': 'off', // Muy común desactivarlo si luego usas TypeScript
     },
   },
-])
+  // Esta línea integra Prettier y apaga las reglas de ESLint que hagan conflicto con el formato
+  prettierPlugin,
+];
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import reactLogo from './assets/react.svg'
-import './App.css'
+import reactLogo from './assets/react.svg';
+import './App.css';
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
         <h1>Hola mundo</h1>
       </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
